c533f07e9165d9d7858a6ca6323d2d35de93a9b7
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-09-26 23:41:15 +0600

    Add github,rsg and ln links
    - Added basic styles for the cards
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,6 +5,13 @@ import { motion } from "framer-motion";
 // StyleSheet
 import "../assets/styles/css/home.min.css";
 
+// Images
+import abirImg from "../assets/images/profile/abir.jpg";
+import emonImg from "../assets/images/profile/emon.jpg";
+import elmaImg from "../assets/images/profile/elma.jpg";
+import sawonImg from "../assets/images/profile/sawon.jpg";
+import urmiImg from "../assets/images/profile/urmi.jpg";
+
 export default function Home() {
   return (
     <motion.div
@@ -23,11 +30,66 @@ export default function Home() {
 
       <section>
         <div className="members-con">
-          <div className="member"></div>
-          <div className="member"></div>
-          <div className="member"></div>
-          <div className="member"></div>
-          <div className="member"></div>
+          <div className="member">
+            <img src={abirImg} alt="Abir" className="img-fluid" />
+            <a href="https://github.com/Abir-Tx">
+              <button>GitHub</button>
+            </a>
+            <a href="">
+              <button>Researchgate</button>
+            </a>
+            <a href="">
+              <button>LinkedIn</button>
+            </a>
+          </div>
+          <div className="member">
+            <img src={elmaImg} alt="Elma" className="img-fluid" />
+            <a href="#">
+              <button>GitHub</button>
+            </a>
+            <a href="">
+              <button>Researchgate</button>
+            </a>
+            <a href="https://www.linkedin.com/in/tarannum-elma-aaab67251/">
+              <button>LinkedIn</button>
+            </a>
+          </div>
+          <div className="member">
+            <img src={sawonImg} alt="Sawon" className="img-fluid" />
+            <a href="https://github.com/sawonmursalin">
+              <button>GitHub</button>
+            </a>
+            <a href="">
+              <button>Researchgate</button>
+            </a>
+            <a href="">
+              <button>LinkedIn</button>
+            </a>
+          </div>
+          <div className="member">
+            <img src={urmiImg} alt="Urmi" className="img-fluid" />
+            <a href="https://github.com/suraiya-akter">
+              <button>GitHub</button>
+            </a>
+            <a href="">
+              <button>Researchgate</button>
+            </a>
+            <a href="https://www.linkedin.com/in/suraiya-akter-40355a251/">
+              <button>LinkedIn</button>
+            </a>
+          </div>
+          <div className="member">
+            <img src={emonImg} alt="Emon" className="img-fluid" />
+            <a href="https://github.com/AlJobair079">
+              <button>GitHub</button>
+            </a>
+            <a href="https://www.researchgate.net/profile/Al-Jobair-Ataur">
+              <button>Researchgate</button>
+            </a>
+            <a href="https://www.linkedin.com/in/al-jobair-emon-0a2557226/">
+              <button>LinkedIn</button>
+            </a>
+          </div>
         </div>
       </section>
     </motion.div>
