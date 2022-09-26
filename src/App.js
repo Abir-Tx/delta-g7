c78bef7d1290352cfb58d6c771bc11c999067eab
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-09-27 00:24:32 +0600

    Add automatic theme switching mechanism
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Footer from "./layouts/footer";
 import Header from "./layouts/header";
 import AnimatedRoutes from "./routes/AnimatedRoutes";
 
+// Automatic theme switching
+window.matchMedia("(prefers-color-scheme: dark)").matches
+  ? document.body.classList.add("dark")
+  : document.body.classList.remove("dark");
+
+// Driver code
 function App() {
   return (
     <div className="App">
