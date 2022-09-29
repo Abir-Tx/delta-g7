e982e2cbb93ad03619800e403c966c81b77f85ad
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-09-29 12:49:03 +0600

    use the json data from details.json file
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- src/components/members.jsx
+++ src/components/members.jsx
@@ -7,6 +7,9 @@ import elmaImg from "../assets/images/profile/elma.jpg";
 import sawonImg from "../assets/images/profile/sawon.jpg";
 import urmiImg from "../assets/images/profile/urmi.jpg";
 
+// Data
+import details from "../assets/data/details.json";
+
 // Stylesheet
 import "../assets/styles/css/members.min.css";
 
@@ -19,29 +22,30 @@ export default function Members() {
             <div className="member">
               <img src={abirImg} alt="Abir" className="img-fluid" />
               <div className="btn-con">
-                <a href="https://github.com/Abir-Tx">
+                <a href={details["abir"][0].github}>
                   <button>GitHub</button>
                 </a>
-                <a href="https://www.researchgate.net/profile/Mushfiqur-Abir">
+                <a href={details["abir"][0].rsg}>
                   <button>Researchgate</button>
                 </a>
-                <a href="https://www.linkedin.com/in/mushfiqur-rahman-abir/">
+                <a href={details["abir"][0].linkedin}>
                   <button>LinkedIn</button>
                 </a>
               </div>
             </div>
           </div>
+
           <div className="col  mx-1">
             <div className="member">
               <img src={elmaImg} alt="Elma" className="img-fluid" />
               <div className="btn-con">
-                <a href="#">
+                <a href={details["elma"][0].github}>
                   <button>GitHub</button>
                 </a>
-                <a href="">
+                <a href={details["elma"][0].rsg}>
                   <button>Researchgate</button>
                 </a>
-                <a href="https://www.linkedin.com/in/tarannum-elma-aaab67251/">
+                <a href={details["elma"][0].linkedin}>
                   <button>LinkedIn</button>
                 </a>
               </div>
@@ -51,13 +55,13 @@ export default function Members() {
             <div className="member">
               <img src={sawonImg} alt="Sawon" className="img-fluid" />
               <div className="btn-con">
-                <a href="https://github.com/sawonmursalin">
+                <a href={details["sawon"][0].github}>
                   <button>GitHub</button>
                 </a>
-                <a href="">
+                <a href={details["sawon"][0].rsg}>
                   <button>Researchgate</button>
                 </a>
-                <a href="https://www.linkedin.com/in/sawon-mursalin-15426624a/">
+                <a href={details["sawon"][0].github}>
                   <button>LinkedIn</button>
                 </a>
               </div>
@@ -68,13 +72,13 @@ export default function Members() {
             <div className="member">
               <img src={urmiImg} alt="Urmi" className="img-fluid" />
               <div className="btn-con">
-                <a href="https://github.com/suraiya-akter">
+                <a href={details["urmi"][0].github}>
                   <button>GitHub</button>
                 </a>
-                <a href="">
+                <a href={details["urmi"][0].rsg}>
                   <button>Researchgate</button>
                 </a>
-                <a href="https://www.linkedin.com/in/suraiya-akter-40355a251/">
+                <a href={details["urmi"][0].linkedin}>
                   <button>LinkedIn</button>
                 </a>
               </div>
@@ -85,13 +89,13 @@ export default function Members() {
             <div className="member">
               <img src={emonImg} alt="Emon" className="img-fluid" />
               <div className="btn-con">
-                <a href="https://github.com/AlJobair079">
+                <a href={details["emon"][0].github}>
                   <button>GitHub</button>
                 </a>
-                <a href="https://www.researchgate.net/profile/Al-Jobair-Ataur">
+                <a href={details["emon"][0].rsg}>
                   <button>Researchgate</button>
                 </a>
-                <a href="https://www.linkedin.com/in/al-jobair-emon-0a2557226/">
+                <a href={details["emon"][0].linkedin}>
                   <button>LinkedIn</button>
                 </a>
               </div>
