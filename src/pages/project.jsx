2e4f973372432571dc3a7c2c1c9830417100680a
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-09-30 12:18:17 +0600

    Create a pdf viewer
    - using `react-pdf` for rendering the pdf inside page
    - REF: https://www.youtube.com/watch?v=GtGmqVyBNVg
    https://github.com/recoding-io/react-videos/blob/main/react-pdf-viewer/src/App.js
    
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -1,5 +1,58 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "../assets/styles/css/project.min.css";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import pdfFile from "../assets/data/pdf/sample.pdf";
 export default function Project() {
-  return <div>Project</div>;
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  function changePage(offSet) {
+    setPageNumber((prevPageNumber) => prevPageNumber + offSet);
+  }
+
+  function changePageBack() {
+    changePage(-1);
+  }
+
+  function changePageNext() {
+    changePage(+1);
+  }
+  return (
+    <div className="project-con">
+      <div className="bg">
+        <div className="title">
+          <h1>Project</h1>
+        </div>
+      </div>
+
+      <div className="desc">
+        <p>
+          <span>W</span>e, Delta G7 have to do a project for this course
+          instructed & under the survailance of our honorable sir Dr. Mohammad
+          Shidujaman. The project must be applied in a real life application or
+          scenario. We are allowed to use <b>Uno, Pico, Rasberry Pi</b> boards
+          for the heart of our project.
+        </p>
+      </div>
+
+      <div className="proposal">
+        <Document file={pdfFile} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+        {pageNumber > 1 && (
+          <button onClick={changePageBack}>Previous Page</button>
+        )}
+        {pageNumber < numPages && (
+          <button onClick={changePageNext}>Next Page</button>
+        )}
+      </div>
+    </div>
+  );
 }
