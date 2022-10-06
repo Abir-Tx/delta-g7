c209e06b83dca097d7344a81fde8cf46593274ea
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-06 16:27:06 +0600

    Copy the style of Projects header to litReview
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

--- src/pages/litReview.jsx
+++ src/pages/litReview.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "../assets/styles/css/litReview.min.css";
 
 export default function LitReview() {
-  return <div className="litRev-con"></div>;
+  return (
+    <motion.div
+      className="litRev-con"
+      initial={{ opacity: 0, scale: 0.7 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0 }}
+      layout
+    >
+      <div className="bg">
+        <div className="title">
+          <h1>Literature Review</h1>
+          <div className="subtitle">
+            <h5>IoT Based Heart Attack Detection</h5>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
 }
