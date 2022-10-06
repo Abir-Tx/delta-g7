acbfc8526f569053b86ad2fe69c3bbf8da089917
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-10-06 18:32:28 +0600

    Make dynamic lit review cards using JSON API
    Signed-off-by: Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>

---
+++ src/components/litReviewCard.jsx
@@ -0,0 +1,51 @@
+import { motion } from "framer-motion";
+import "../assets/styles/css/litReviewCard.min.css";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import { useState } from "react";
+import { useEffect } from "react";
+import backupDetails from "../assets/data/details.json";
+
+export default function LitReviewCard(props) {
+  const [details, setTest] = useState(backupDetails);
+  useEffect(() => {
+    fetch("https://jsonblob.com/api/jsonBlob/1026725589796798464")
+      .then((response) => response.json())
+      .then((data) => setTest(data))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  return (
+    <div className="litRevCard-con">
+      <div className="title-con">
+        <img
+          src={props.image}
+          alt={details[props.refName][0].name}
+          className="title-img"
+        />
+        <h2>{details[props.refName][0].name}</h2>
+      </div>
+
+      <div className="pdf-con">
+        <Document file={props.pdf}>
+          <Page pageNumber={1} height={500} />
+        </Document>
+
+        <div className="revText-con">
+          <h4>Short Summary</h4>
+          <p>{props.review}</p>
+
+          <div className="ref-con">
+            <h4>References</h4>
+
+            {Object.keys(details[props.refName][0]["refs"][0]).map((key, i) => (
+              <a href={details[props.refName][0]["refs"][0][key]}>
+                <div className="btn btn-primary p-2 m-2">{key}</div>
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/litReview.jsx
+++ src/pages/litReview.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { motion } from "framer-motion";
 import "../assets/styles/css/litReview.min.css";
+import LitReviewCard from "../components/litReviewCard";
+import abirImg from "../assets/images/profile/abir.jpg";
+import pdfFile from "../assets/data/pdf/project-proposal.pdf";
 
 export default function LitReview() {
   return (
@@ -18,6 +21,22 @@ export default function LitReview() {
             <h5>IoT Based Heart Attack Detection</h5>
           </div>
         </div>
+      </div>
+
+      <div className="cards">
+        <LitReviewCard
+          image={abirImg}
+          pdf={pdfFile}
+          review="This is a test review"
+          refName="abir"
+        />
+
+        <LitReviewCard
+          image={abirImg}
+          pdf={pdfFile}
+          review="Hey this is a test"
+          refName="elma"
+        />
       </div>
     </motion.div>
   );
