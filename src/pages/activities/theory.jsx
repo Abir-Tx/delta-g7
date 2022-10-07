2d94d6671e080f068fd3c1af17cedffbac0fe5c8
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2022-10-08 00:01:50 +0600

    Create theory page with title
    Signed-off-by: Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>

--- src/pages/activities/theory.jsx
+++ src/pages/activities/theory.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "../../assets/styles/css/theory.min.css";
 
 export default function Theory() {
-  return <div>Theory</div>;
+  return (
+    <div className="theory-con">
+      <div className="title">
+        <h2>Theory Activities</h2>
+      </div>
+    </div>
+  );
 }
